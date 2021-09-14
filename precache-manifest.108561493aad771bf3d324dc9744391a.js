e23f7ad0004dd694462a65d5527e910c4f8f98ed
Jose Ibarra <jibarra8917@gmail.com>
2021-09-13 22:45:14 -0600

    Updates

---
+++ precache-manifest.108561493aad771bf3d324dc9744391a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf5e0a83e869a0a0bc52bebe5450b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6494fe05485e80af2f62",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "88448ffcfce5fdaf8e09",
+    "url": "/static/js/2.cfafeff4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.cfafeff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6494fe05485e80af2f62",
+    "url": "/static/js/main.ac559ce6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "3d005e2895e1ad7b81505d59f0decd06",
+    "url": "/static/media/ProfilePic250px.3d005e28.jpeg"
+  },
+  {
+    "revision": "930af95e4de3db9744a62bf0f63c1e7e",
+    "url": "/static/media/resume.930af95e.pdf"
+  }
+]);
